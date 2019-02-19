e660bfe0e046bc88c2f790bdb8d613e913af66c2
Sungjun <sungjunpizz@gmail.com>
2019-02-19 09:21:09 +0900

    First Commit : Hello world

---
+++ src/main/java/com/sungjun/springbootmagicuser/SpringbootmagicuserApplication.java
@@ -0,0 +1,13 @@
+package com.sungjun.springbootmagicuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmagicuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmagicuserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sungjun/springbootmagicuser/SpringbootmagicuserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sungjun.springbootmagicuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmagicuserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
